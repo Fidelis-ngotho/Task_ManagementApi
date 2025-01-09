104d24ccf205da0b8a2d657301acaf05b486451e
Fidelis-ngotho <fidelngotho2@gmail.com>
2025-01-09 09:35:21 +0300

    add all changes

--- Task_management_api/settings.py
+++ Task_management_api/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--imvw0=3q)0cuka5c=z=q$xz_trx1b23^##loh6$f5fkh3bbfe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = [Task_management-api.up.railway.app ]
 
 
 # Application definition
